f03c12604a673ab00412948c2eb12ef1fb283055
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-01-14 14:52:37 +1100

    code

--- contact.php
+++ contact.php
@@ -10,20 +10,35 @@
 <h1>Heading</h1>
 
 <div class="container-fluid">
-    <form>
+    <form action="contact.php" method="post">
         <div class="mb-3">
             <label for="inputEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="inputMessage" class="form-label">Message</label>
-            <input type="text" class="form-control" id="inputMessage">
+            <input type="text" class="form-control" id="inputMessage" name="inputMessage">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emailAddress = $_POST['inputEmail'];
+    $messageSubmitted = $_POST['inputMessage'];
+
+    echo $emailAddress;
+    echo "<p>";
+    echo $messageSubmitted;
+}
+?>
+
 </body>
 <script src="js/bootstrap.bundle.min.js"></script>
 </html>
+
+
+
+
