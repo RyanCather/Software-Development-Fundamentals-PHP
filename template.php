e530b63768888b15e36ff60af30f2edcc1a3e8fb
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-01-16 10:34:50 +1100

    init for sanitising data

---
+++ template.php
@@ -0,0 +1,13 @@
+<?php
+
+function sanitiseData($unsanitisedData)
+{
+    $unsanitisedData = trim($unsanitisedData);
+    $unsanitisedData = stripslashes($unsanitisedData);
+    $sanitisedData = htmlspecialchars($unsanitisedData);
+    return $sanitisedData;
+}
+
+?>
+
+
