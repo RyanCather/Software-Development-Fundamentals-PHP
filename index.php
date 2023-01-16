f485bd2ff75cb2b7d2a27060a88b4ca3e580e013
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-01-16 14:02:58 +1100

    footer code

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-echo "Hello World";
+<!doctype html>
+<?php include "template.php"?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Heading</h1>
+
+
+<?php echo footer() ?>
+</body>
+<script src="js/bootstrap.bundle.min.js" ></script>
+</html>
+
--- template.php
+++ template.php
@@ -1,13 +1,21 @@
 <?php
 
-function sanitiseData($unsanitisedData)
+function footer(): string
 {
-    $unsanitisedData = trim($unsanitisedData);
-    $unsanitisedData = stripslashes($unsanitisedData);
-    $sanitisedData = htmlspecialchars($unsanitisedData);
-    return $sanitisedData;
+    date_default_timezone_set('Australia/Canberra');
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
 }
+
 
 ?>
 
 
+<!--function sanitiseData($unsanitisedData)-->
+<!--{-->
+<!--    $unsanitisedData = trim($unsanitisedData);-->
+<!--    $unsanitisedData = stripslashes($unsanitisedData);-->
+<!--    $sanitisedData = htmlspecialchars($unsanitisedData);-->
+<!--    return $sanitisedData;-->
+<!--}-->
